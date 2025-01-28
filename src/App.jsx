7a708f0b51de79c76db20302e0e6b259fa631381
OhExtesyyy <oleg2006king@gmail.com>
2025-01-28 19:46:27 +0300

    Baza

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+export default function App() {
+  return (
+    <div class='wrapper'>
+      <Header />
+      <div class='content'>
+        <div class='container'>
+          <div class='content__top'>
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class='content__title'>Все пиццы</h2>
+          <div class='content__items'>
+            <PizzaBlock title='Мексиканская' price="500" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
