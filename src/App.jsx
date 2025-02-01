a9c4d12c9f90252fc351b04bbce51bfdd21d52b8
OhExtesyyy <oleg2006king@gmail.com>
2025-02-01 17:00:01 +0300

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import './scss/app.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
-import NoyFound from './pages/NotFound';
+import NotFound from './pages/NotFound';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 export default function App() {
-
-
   return (
     <div className='wrapper'>
       <Header />
       <div className='content'>
         <div className='container'>
-          <Home />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+          
         </div>
       </div>
     </div>
--- src/components/NotFoundBlock/Index.jsx
+++ src/components/NotFoundBlock/Index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import styles from "./NotFoundBlock.module.scss"
 
 export default function NotFoundBlock() {
   return (
-    <div>
+    <div className={styles.root}> 
       <h1>
         <span>😖</span> 
         <br />
         Ничего не найдено
       </h1>
+      <p className={styles.description}>Приносим извинения, к сожалению данной страница отсутствует на нашем сайте</p>
     </div>
   );
 }
